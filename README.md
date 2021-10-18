ca044ad77bc4bb5b1ec43f0730558f21b33337d7
erlendoeien <pappegoyen1@gmail.com>
2021-10-18 17:52:31 +0200

    Fix multiple downloads

--- README.md
+++ README.md
@@ -8,4 +8,12 @@ This is a course project for to learn
 
 We will attempt to create a model which can distinguish the art styles of Marvel comics from DC comics.
 
-_add more later on_
+# How to use `get_data.py`
+1. Make sure you have python installed (tested with v3.8.3), and preferably a virtual environemnt
+2. Install dependencies, e.g. with pip: `pip install -r requirements.txt`
+3. Find the comic book you would like to fetch, from https://readcomicsonline.ru/
+4. Get the name and the chapter (edition) of the comic from the URL, e.g. `immortal-hulk-2018` and chapter `1` in the url
+https://readcomicsonline.ru/comic/immortal-hulk-2018/1/1.
+The URLs are always on the format `https://readcomicsonline.ru/comic/<comic-name>/<chapterNr>/<pageNr>`.
+There are some examples in the script on the format the comic book name and chapter should be added to the script
+5. Run the script and await for the images to be downloaded
--- get_data.py
+++ get_data.py
@@ -1,16 +1,24 @@
 import aiohttp
 import asyncio
 import copy
+import os
 from io import BytesIO
 from PIL import Image
 
 
 def initialize():
-    dc, marvel = [{"name": "batman-2016", "chapter": 1, "imgs": {}},{"name": "the-flash-2016", "chapter":1, "imgs":{}}], [{"name": "immortal-hulk-2018", "chapter": 1, "imgs": {}}] 
+    """Hardcoded initialize function where one can supply
+    the wanted comic books by name and their 'chapter' (edition)"""
+    dc, marvel = [
+        {"name": "batman-2016", "chapter": 1, "imgs": []},
+        {"name": "the-flash-2016", "chapter": 1, "imgs": []},
+    ], [{"name": "immortal-hulk-2018", "chapter": 1, "imgs": []}]
     return dc, marvel
 
 
 def get_url(comic_name: str, chapter: int, page: int):
+    """Maps the comic book page to the correct image url. The comic book name is retrieved from the url
+    of the targeted comic book."""
     str_page = "0" + str(page) if page < 10 else page
     return f"https://readcomicsonline.ru/uploads/manga/{comic_name}/chapters/{chapter}/{str_page}.jpg"
 
@@ -18,6 +26,7 @@ def get_url(comic_name: str, chapter: int, page: int):
 async def get_comic_page(
     session: aiohttp.ClientSession, name: str, chapter: int, page: int, **kwargs
 ) -> bytes:
+    """Fetches a single comic book page, asynchronous"""
     url = get_url(name, chapter, page)
     print(f"Requesting {url}")
     resp = await session.request("GET", url=url, **kwargs)
@@ -27,13 +36,17 @@ async def get_comic_page(
         return data
     return None
 
+
 def stream_to_img(stream: bytes):
-    return Image.open(BytesIO(stream)) if stream else None
+    """Converts a image byte stream to a PIL Image-object"""
+    return Image.open(BytesIO(stream))
 
 
 async def get_comic_book(comic: dict, session: aiohttp.ClientSession, **kwargs):
     # TODO: Mutating the comic-dict -> Refactor all code to be non-mutuable
+    copy_comic = copy.deepcopy(comic)
     page_tasks = []
+    # Creates the async takes to be executed
     for page in range(1, 50):  # assuming maximum comic book length
         page_tasks.append(
             get_comic_page(
@@ -44,46 +57,68 @@ async def get_comic_book(comic: dict, session: aiohttp.ClientSession, **kwargs):
                 **kwargs,
             )
         )
+    # Iterates over each async task to store only valid pages
     for task in asyncio.as_completed(page_tasks):
         try:
             response = await task
-            if not response:
-                continue
-            comic["imgs"][response] = response
+            if response:
+                copy_comic["imgs"].append(response)
         except Exception as e:
             print(f"Last page before exception {page-1}")
             print(e)
+    return copy_comic
 
 
-async def get_comics(dc_comics: list, marvel_comics: list, **kwargs):
-    dc, marvel = copy.deepcopy(dc_comics), copy.deepcopy(marvel_comics)
+async def get_comics(comics: list, session: aiohttp.ClientSession, **kwargs):
+    """Fetches each page for each comic book dictionary supplied in the list"""
+    comics_copy = []
+    for comic in comics:
+        comics_copy.append(await get_comic_book(comic=comic, session=session, **kwargs))
+    return comics_copy
+
+
+async def get_all_comics(dc_comics: list, marvel_comics: list, **kwargs):
+    """Fetches each page for each comic book dictionary supplied
+    As it is suggested to use the same aiohttp-session, there is some red"""
+    dc, marvel = [], []
     async with aiohttp.ClientSession() as session:
-        for comic in dc:
-            await get_comic_book(comic=comic, session=session, **kwargs)
-        for comic in marvel:
-            await get_comic_book(comic=comic, session=session, **kwargs)
+        dc = await get_comics(dc_comics, session, **kwargs)
+        marvel = await get_comics(marvel_comics, session, **kwargs)
     return dc, marvel
 
 
 async def fetch_data(**kwargs):
+    """Initializes the targeted comic books and fetches them"""
     dc, marvel = initialize()
-    dc, marvel = await get_comics(dc_comics=dc, marvel_comics=marvel, **kwargs)
+    async with aiohttp.ClientSession() as session:
+        dc = await get_comics(dc, session, **kwargs)
+        marvel = await get_comics(marvel, session, **kwargs)
     return dc, marvel
 
 
 def save_images(comics: list, dir_name: str):
-    imgs = [stream_to_img(stream) for stream in comics[0]["imgs"]]
-    for idx, img in enumerate(imgs, start=1):
-        if img:
-            img.save(f"./project/data/{dir_name}/page_async_{idx}.jpg")
+    """Stores the comic books in directories by their comic book name,
+    one JPG-file for each comic book page.
+    NB: Often the 1-2 first and last pages are not actual 'comics'
+    and might need to be excluded"""
+    for comic in comics:
+        imgs = [stream_to_img(stream) for stream in comic["imgs"] if not None]
+        dir_path = os.path.join(
+            os.path.realpath("data"), dir_name, f"{comic['name']}-{comic['chapter']}"
+        )
+        os.makedirs(dir_path, exist_ok=True)
+        for idx, img in enumerate(imgs, start=1):
+            if img:
+                img.save(os.path.join(dir_path, f"page_async_get_{idx}.jpg"))
 
 
-async def main():
-    dc_comics, marvel_comics = await fetch_data(ssl=False)
+def main():
+    dc_comics, marvel_comics = asyncio.get_event_loop().run_until_complete(
+        fetch_data(ssl=False)
+    )
     save_images(dc_comics, "DC")
     save_images(marvel_comics, "Marvel")
 
 
-
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    main()
