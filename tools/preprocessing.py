a26034d5f44c93c180e325e299507872ced39b78
rechatin <hugo.rechatin@eurecom.fr>
2022-01-14 10:42:07 +0100

    i did 4 main modifications : the first, when calling the load all images function, by adding a True boolean in the arguments, you will load grayscale images, second and third, when calling crop_image_to_sample_size, you can also add a true boolean after all the arguments which will then trigger an if statment in crop_from_tlc to either passe the images trhough a Laplacian filter or pass them trhough histogram equilization (to do one and not the other, you comment/uncomment the bit of code in the if statment). Fourth, I added a additional crop_image_to_sample_size_4_rot function, that calls a crop_from_tlc_4_rot function that save four 90 degree rotations of each crop.

--- tools/preprocessing.py
+++ tools/preprocessing.py
@@ -21,17 +21,25 @@ def load_images_from_folder(folder):
     return images
 
 
-def load_all_images(img_category_folder):
+def load_all_images(img_category_folder, gray=False):
     imgs_flattened = []
     for comic_folder_path, subdirs, pages in os.walk(img_category_folder):
         print(comic_folder_path)
-        imgs_flattened.extend(
-            [
-                cv2.imread(os.path.join(comic_folder_path, page))
-                for page in pages
-                if page.endswith(".jpg")
-            ]
-        )
+        if(gray == True):
+            imgs_flattened.extend(
+                [           
+                    cv2.imread(os.path.join(comic_folder_path, page), 0)
+                    for page in pages
+                    if page.endswith(".jpg")
+                ])
+        else:
+            imgs_flattened.extend(
+                [           
+                    cv2.imread(os.path.join(comic_folder_path, page))
+                    for page in pages
+                    if page.endswith(".jpg")
+                ])
+        
     return imgs_flattened
 
 
@@ -46,10 +54,16 @@ def reshape_to_uniform_dim(images):
 # d : dimension of the square crop
 # i : the index allowing us to differentiate all samples that we save
 # in imwrite we can specify the folder were we want to save all the data
-def crop_from_tlc(image, tlc, d, i, output_dir):
+def crop_from_tlc(image, tlc, d, i, output_dir, flag = False):
     im = image[tlc[0] : tlc[0] + d - 1, tlc[1] : tlc[1] + d - 1]
     if im.size != 0:
         im = np.uint8(im)
+        if(flag == True):
+            imgYCC = cv2.cvtColor(im, cv2.COLOR_RGB2YCR_CB) #color image histogramme eq  
+            imgYCC[:,:,0] = cv2.equalizeHist(imgYCC[:,:,0])
+            im = cv2.cvtColor(imgYCC, cv2.COLOR_YUV2BGR)
+            # im = cv2.bilateralFilter(im, 15, 85, 85) #Laplacian Filter
+            # im = cv2.Laplacian(im, cv2.CV_16S, 1)
         cv2.imwrite(os.path.join(output_dir, f"sample_{str(i)}.png"), im)
 
 def crop_image_by_width(image, res):
@@ -77,15 +91,11 @@ def crop_image_by_width(image, res):
         y += res
     return np.array(samples, dtype="float")
 
-
-
-    
-
-
 # images : the list of all images we want to crop into samples
 # n : the number of crops we do for a given image
 # x : the dimansion of the square crop
-def crop_images_to_sample_size(images, d, n, output_dir):
+
+def crop_images_to_sample_size(images, d, n, output_dir, flag = False):
     l = 0
     Path(output_dir).mkdir(parents=True, exist_ok=True)
     for i in range(len(images)):
@@ -100,8 +110,40 @@ def crop_images_to_sample_size(images, d, n, output_dir):
             )  # initialized in the zone where the crop would step out
             tlc[0] = rand_y
             tlc[1] = rand_x
-            crop_from_tlc(images[i], tlc, d, l, output_dir)
+            if(flag == True):
+                crop_from_tlc(images[i], tlc, d, l, output_dir, True)
+            else:
+                crop_from_tlc(images[i], tlc, d, l, output_dir)
             l += 1
+
+def crop_from_tlc_4_rot(image, tlc, d, i, output_dir):
+    im = image[tlc[0] : tlc[0] + d - 1, tlc[1] : tlc[1] + d - 1]
+    if im.size != 0:
+        im = np.uint8(im)
+        rows,cols = im.shape[0:2]
+        center = (cols/2,rows/2)
+        rotate = cv2.getRotationMatrix2D(center,90,1.0)
+        for k in range(4):
+            cv2.imwrite(os.path.join(output_dir, f"sample_{str(i+k)}.png"), im)
+            im_rot = cv2.warpAffine(im, rotate, (cols,rows), flags=cv2.INTER_LINEAR)
+            im = im_rot
+            
+
+def crop_images_to_sample_size_4_rot(images, d, n, output_dir):
+    l = 0
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    for i in range(len(images)):
+        shape = images[i].shape
+        tlc = [0, 0]
+        for k in range(0, n):
+            rand_x = np.uint16(random.uniform(0, shape[1] - d))  # I make sure here that the top left corner cannot be
+            rand_y = np.uint16(random.uniform(0, shape[0] - d))  # initialized in the zone where the crop would step out
+            tlc[0] = rand_y
+            tlc[1] = rand_x
+            crop_from_tlc_4_rot(images[i], tlc, d, l, output_dir)
+            l += 4
+
+
 
 
 if __name__ == "__main__":
@@ -110,8 +152,6 @@ if __name__ == "__main__":
     marvel_imgs = load_all_images(os.path.join(data_path, "Marvel"))
     dc_imgs = load_all_images(os.path.join(data_path, "DC"))
 
-    processed_path = os.path.join(data_path, "processed", "crop_6_1000x1000")
-    # Crop all images into 6 samples given the page resolution of 3056x1988, each of resolution 1000x1000 (-1 so in fact 999x999)
-    # To minimize overlap between samples
-    crop_images_to_sample_size(marvel_imgs, 1000, 6, os.path.join(processed_path, "marvel"))
-    crop_images_to_sample_size(dc_imgs, 1000, 6, os.path.join(processed_path, "dc"))
+    processed_path = os.path.join(data_path, "processed", "rot_test")
+    crop_images_to_sample_size_4_rot(marvel_imgs, 1000, 1, os.path.join(processed_path, "marvel"))
+    crop_images_to_sample_size(dc_imgs, 1000, 6, os.path.join(processed_path, "dc"), True)
